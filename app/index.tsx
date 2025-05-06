4320132a8505f65199128b0252c6f0c26b99c412
Aadit-001 <developer.aj_001@outlook.com>
2025-05-06 14:07:36 +0530

    Invitation App making start

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import "./global.css";
+
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-red-500"
+    >
+      <Text className="text-6xl font-bold">Hello World</Text>
+    </View>
+  );
+}
